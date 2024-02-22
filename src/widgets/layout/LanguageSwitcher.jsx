5b2eb4eaa946132c630ead729dce25aa25522ae8
NattakarnPreesong <88360816+NattakarnPreesong@users.noreply.github.com>
2024-02-22 09:18:55 +0700

    Add files via upload

--- src/widgets/layout/LanguageSwitcher.jsx
+++ src/widgets/layout/LanguageSwitcher.jsx
@@ -10,17 +10,26 @@ const LanguageSwitcher = () => {
 
   return (
     <div className="flex items-center">
-        <button
+      <button
         className={`mr-2 ${i18n.language === "en" ? "selected" : ""}`}
         onClick={() => changeLanguage("en")}
+        style={{ margin: "0 5px" }}
       >
         EN
       </button>
       <button
         className={`${i18n.language === "jp" ? "selected" : ""}`}
         onClick={() => changeLanguage("jp")}
+        style={{ margin: "0 5px" }}
       >
         JP
+      </button>
+      <button
+        className={`${i18n.language === "th" ? "selected" : ""}`}
+        onClick={() => changeLanguage("th")}
+        style={{ margin: "0 5px" }}
+      >
+        TH
       </button>
     </div>
   );
